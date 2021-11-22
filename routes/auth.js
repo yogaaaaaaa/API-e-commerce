8d18493c7a5d8494b210dfa428c65ee3c97b6aaf
yoga adhipratama <yogaadhi@icloud.com>
2021-11-22 23:02:30 +0700

    auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
+const CryptoJS = require("crypto-js");
 
 //Registration
 router.post("/registration", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
+
   try {
     const savedUser = await newUser.save();
-    res.status(201).json(savedUser)
+    res.status(201).json(savedUser);
+    console.log(savedUser);
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password && res.status(401).json("wrong credentials");
+
+    const {password, ...others} = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 module.exports = router;
