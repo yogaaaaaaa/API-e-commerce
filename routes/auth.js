c019265661d95a8280df90a5679406d110f9006b
adhipratama <yogaadhi@icloud.com>
2021-11-23 09:56:16 +0700

    commit in windows

--- routes/auth.js
+++ routes/auth.js
@@ -29,17 +29,21 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     !user && res.status(401).json("wrong credentials!");
 
-    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
 
     const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    OriginalPassword !== req.body.password && res.status(401).json("wrong credentials");
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("wrong credentials");
 
-    const {password, ...others} = user._doc;
+    const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
-  } 
+  }
 });
 
 module.exports = router;
